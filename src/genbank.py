80744ce7f6e8da5d0f3ccec10cbf867f3501ed63
Jamie <jamietmorton@gmail.com>
2013-10-10 18:16:53 -0400

    Modified genbank dictionaries

--- src/genbank.py
+++ src/genbank.py
@@ -11,23 +11,50 @@ import os
 import argparse
 
 class GenBank(object):
-    def __init__(self,fname):
+    def __init__(self,fname,dicttype="gene"):
         self.fname = fname
-        self.genes = dict()
+        if dicttype=="protein":
+            self.dict = self.buildProteinDictionary(fname)
+        else:
+            self.dict = self.buildGeneDictionary(fname)
 
+
+    def buildProteinDictionary(self,fname):
+        proteins = dict()
+        print fname
+        seq_record = SeqIO.parse(open(fname), "genbank").next()
+        for feature in seq_record.features:
+            if feature.type == 'CDS':
+                try:
+                    protein = feature.qualifiers["protein_id"][0]
+                    proteins[ protein ]=feature
+                except KeyError:
+                    continue
+        return proteins
+
+    def buildGeneDictionary(self,fname):
+        genes = dict()
         seq_record = SeqIO.parse(open(fname), "genbank").next()
         for feature in seq_record.features:
             if feature.type == 'CDS':
                 try:
                     gene = feature.qualifiers["gene"][0]
-                    self.genes[ gene ]=feature
+                    genes[ gene ]=feature
                 except KeyError:
                     continue
+        return genes
+
     def findGene(self,gname):
         try:
-            return self.genes[gname]
+            return self.dict[gname]
         except KeyError:
             print >> sys.stderr,"No such gene"
+
+    def findProtein(self,pname):
+        try:
+            return self.dict[pname]
+        except KeyError:
+            print >> sys.stderr,"No such protein"
 
 if __name__=="__main__":
     import unittest
--- src/test.py
+++ src/test.py
@@ -5,6 +5,11 @@ A set of test functions for unittests
 
 def createFasta(fname,refid,refseq):
     fastaH = open(fname,'w')
+    fastaH.write(">%s\n%s\n"%(refid,refseq))
+    fastaH.close()
+    
+def createFastaIdx(fname,refid,refseq):
+    fastaH = open(fname,'w')
     fastaIdx = open(fname+".fai",'w')
     fastaH.write(">%s\n%s\n"%(refid,refseq))
     fastaIdx.write("%s\t%d\t%d\t%d\t%d\n"%(refid,len(refseq),len(refid)+2,len(refseq),len(refseq)+1))
