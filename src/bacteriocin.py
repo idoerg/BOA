b108fb1d46e9e940e13ce20d0b9bfe2a9f842c5f
Jamie <jamietmorton@gmail.com>
2013-10-14 01:33:54 -0400

    Made bacteriocin more modular

--- src/bacteriocin.py
+++ src/bacteriocin.py
@@ -19,49 +19,26 @@ import numpy
 import re
 import subprocess
 
-parser = argparse.ArgumentParser(description=\
-    'Finds intergenic regions from genback file')
-parser.add_argument(\
-    '--genes', type=str,required=True,default="",
-    help='A FASTA file containing all of the genes of interest')
-parser.add_argument(\
-    '--genbank-files', type=str, nargs="+", required=False,
-    help='The genbank files containing annotated genes')
-parser.add_argument(\
-    '--bacteriocins', type=str, required=True,
-    help='The bacteriocin proteins that are to be blasted')
-parser.add_argument(\
-    '--radius', type=int, required=False, default=10000,
-    help='The search radius around every specified gene')
-parser.add_argument(\
-    '--evalue', type=float, required=False, default=0.00001,
-    help='The search radius around every specified gene')
-parser.add_argument(\
-    '--intermediate', type=str, required=True,
-    help='Directory for storing intermediate files')
-parser.add_argument(\
-    '--verbose', action='store_const', const=True, default=False,
-    help='Messages for debugging')
-parser.add_argument(\
-    '--test', action='store_const', const=True, default=False,
-    help='Run unittests')
 
 import genbank
 import blast
 import intergene
 
-blast.addArgs(parser)
-args = parser.parse_args()
 
 loc_reg = re.compile("(\d+):(\d+)")
 
 class IntergeneHandler:
-    def __init__(self,genbank_files,input_genes,intermediate):
+    def __init__(self,genbank_files,input_genes,intermediate,evalue,num_threads,radius,verbose,keep_tmp):
         self.pid = os.getpid() #Use current pid to name temporary files
         self.genbank_files = genbank_files
         self.genomic_query = "%s/genomicQuery.%d.fa"%(intermediate,self.pid)
         self.intergene_file = "%s/intergenes.%d.fasta"%(intermediate,self.pid)
         self.target_genes = "%s/target_genes.%d.fasta"%(intermediate,self.pid)
+        self.evalue = evalue
+        self.num_threads = num_threads
+        self.radius = radius
+        self.verbose = verbose
+        self.keep_tmp = keep_tmp
         SeqIO.write(input_genes,self.target_genes,"fasta")
 
         self.intermediate = intermediate
@@ -100,14 +77,14 @@ class IntergeneHandler:
 
     Right it only considers the first hit
     """
-    def getUnannotatedIntergenes(self,genbank_file,protein_file):
+    def getUnannotatedIntergenes(self,genbank_file,protein_file,evalue,num_threads,radius,verbose,keep_tmp):
         intergenes = []
         proteinDict =  genbank.GenBank(genbank_file,"protein")
-        blast_obj = blast.BLAST(protein_file,self.target_genes,self.intermediate,args.evalue)
+        blast_obj = blast.BLAST(protein_file,self.target_genes,self.intermediate,evalue)
         blast_obj.buildDatabase("protein")
-        blast_obj.run(blast_cmd="blastp",mode="xml",num_threads=args.num_threads)
-        if args.verbose: print >> sys.stderr,blast_obj.formatDBCommand("protein")
-        if args.verbose: print >> sys.stderr,blast_obj.BLASTCommand("blastp",num_threads=args.num_threads)
+        blast_obj.run(blast_cmd="blastp",mode="xml",num_threads=num_threads)
+        if verbose: print >> sys.stderr,blast_obj.formatDBCommand("protein")
+        if verbose: print >> sys.stderr,blast_obj.BLASTCommand("blastp",num_threads=num_threads)
         blast_file = blast_obj.getFile()
         blast_qresults = SearchIO.read(blast_file,'blast-xml')
         organism = os.path.dirname(genbank_file)
@@ -120,8 +97,8 @@ class IntergeneHandler:
             protein_record = proteinDict.findProtein(protein_id)
             geneSt, geneEnd = loc_reg.findall(str(protein_record.location))[0]
             geneSt,geneEnd = int(geneSt), int(geneEnd)
-            intergenes += self.filterIntergenes(self.intergene_file,geneSt,geneEnd,args.radius)
-        if not args.keep_tmp:
+            intergenes += self.filterIntergenes(self.intergene_file,geneSt,geneEnd,radius)
+        if not keep_tmp:
             blast_obj.cleanup()
         return intergenes
 
@@ -139,29 +116,76 @@ class IntergeneHandler:
             try:
                 sequences = SeqIO.parse(gbk, "genbank")
                 intergene.get_interregions(gbk,self.intergene_file)
-                intergenic_regions=self.getUnannotatedIntergenes(gbk,protein_file)
+                intergenic_regions=self.getUnannotatedIntergenes(gbk,protein_file,
+                                                                 self.evalue,
+                                                                 self.num_threads,
+                                                                 self.radius,
+                                                                 self.verbose,
+                                                                 self.keep_tmp)
                 SeqIO.write(intergenic_regions,intergene_handle,"fasta")
             except IOError:
                 print>> sys.stderr,".faa folder must be in the same folder as the genbank file"
         intergene_handle.close()
 
-def go():
+def go(genbank_files,bacteriocins,genes,output_file,intermediate,evalue,num_threads,radius,verbose,keep_tmp):
     intergenes = []
-    input_genes = [x for x in SeqIO.parse(args.genes,"fasta")]
-    intergene_obj = IntergeneHandler(args.genbank_files,input_genes,args.intermediate)
+    input_genes = [x for x in SeqIO.parse(genes,"fasta")]
+    intergene_obj = IntergeneHandler(genbank_files,input_genes,
+                                     intermediate,evalue,
+                                     num_threads,radius,
+                                     verbose,keep_tmp)
     intergene_obj.buildIntergenicDatabase()
     intergeneFile = intergene_obj.getGenomicQuery()
-    blast_obj = blast.BLAST(intergeneFile,args.bacteriocins,args.intermediate,args.evalue)
+    blast_obj = blast.BLAST(intergeneFile,bacteriocins,intermediate,evalue)
     blast_obj.buildDatabase("nucleotide")
-    blast_obj.run(blast_cmd="tblastn",mode="tab",num_threads=args.num_threads)
-    if args.verbose: print >> sys.stderr,blast_obj.formatDBCommand("nucleotide")
-    if args.verbose: print >> sys.stderr,blast_obj.BLASTCommand("tblastn",num_threads=args.num_threads)
+    blast_obj.run(blast_cmd="tblastn",mode="tab",num_threads=num_threads)
+    if verbose: print >> sys.stderr,blast_obj.formatDBCommand("nucleotide")
+    if verbose: print >> sys.stderr,blast_obj.BLASTCommand("tblastn",num_threads=num_threads)
     hits = blast_obj.parseBLAST("tab")
-    outHandle = open(args.output_file,'w')
+    outHandle = open(output_file,'w')
     outHandle.write("\n".join( map( str, hits)))
-    if not args.keep_tmp:
+    if not keep_tmp:
         blast_obj.cleanup()
         intergene_obj.cleanup()
 
 if __name__=="__main__":
-    go()
+    parser = argparse.ArgumentParser(description=\
+        'Finds intergenic regions from genback file')
+    parser.add_argument(\
+        '--genes', type=str,required=True,default="",
+        help='A FASTA file containing all of the genes of interest')
+    parser.add_argument(\
+        '--genbank-files', type=str, nargs="+", required=False,
+        help='The genbank files containing annotated genes')
+    parser.add_argument(\
+        '--bacteriocins', type=str, required=True,
+        help='The bacteriocin proteins that are to be blasted')
+    parser.add_argument(\
+        '--radius', type=int, required=False, default=10000,
+        help='The search radius around every specified gene')
+    parser.add_argument(\
+        '--evalue', type=float, required=False, default=0.00001,
+        help='The search radius around every specified gene')
+    parser.add_argument(\
+        '--intermediate', type=str, required=True,
+        help='Directory for storing intermediate files')
+    parser.add_argument(\
+        '--verbose', action='store_const', const=True, default=False,
+        help='Messages for debugging')
+    parser.add_argument(\
+        '--test', action='store_const', const=True, default=False,
+        help='Run unittests')
+
+    blast.addArgs(parser)
+    args = parser.parse_args()
+
+    go(args.genbank_files,
+       args.bacteriocins,
+       args.genes,
+       args.output_file,
+       args.intermediate,
+       args.evalue,
+       args.num_threads,
+       args.radius,
+       args.verbose,
+       args.keep_tmp)
