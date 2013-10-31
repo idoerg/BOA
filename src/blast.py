193d5a7155e20bd3f2bed43984b83873f1fb9341
jamie <jamie@ubuntu.(none)>
2013-10-31 12:54:46 -0400

    Added query and sbjct id handling in blast

--- src/blast.py
+++ src/blast.py
@@ -33,50 +33,88 @@ def addArgs(parser):
         '--num-threads', type=int, required=False, default=1,
         help='The number of threads to be run on blast')
 
-location_reg = re.compile("\d+:(\d+)-(\d+)")
+location_reg = re.compile(":(\d+)-(\d+)(\S)")
 locus_reg = re.compile("locus:(\d+)-(\d+)(\S)")
 
-class XMLRecord():
+class  XMLRecord(object):
     """
+
     Records information for each blast entry that is important for multiple alignment
     """
     def __init__(self,
-                 record_number,
                  description,
                  expected_value,
                  score,
                  query,             #Sequence from database
+                 query_id,           
                  query_start,
                  query_end,
                  sbjct,             #Sequence that was blasted
+                 sbjct_id,           
                  sbjct_start,
                  sbjct_end):
-        self.record_number = record_number
         self.description = description
         self.score = score
         self.expected_value = expected_value
         self.query = query
+        self.query_id = query_id
         self.query_start = query_start
         self.query_end = query_end
         self.sbjct = sbjct
+        self.sbjct_id = sbjct_id
         self.sbjct_start = sbjct_start
         self.sbjct_end = sbjct_end
+        
 
-    def __str__(self):
-
-        string = '****Alignment****\n' \
-          +'Record:%d\n'%self.record_number \
-          +'%s:\n'%self.description \
-          +'e value:%lf\n'%self.expected_value \
-          +'Score:%lf\n'%self.score \
-          +'Query:\t%s\t%s\t%s\n'%(self.query_start,self.query,self.query_end) \
-          +'Sbjct:\t%s\t%s\t%s\n'%(self.sbjct_start,self.sbjct,self.sbjct_end)
-        return string
-
-class TabRecord():
+class CoordXMLRecord(XMLRecord):
     """
     Records information for each blast entry that is important for multiple alignment
+    Only used for blasting bacteriocins where the coordinates in the id is important
+
+    Assumption: the id's have genome coordinates embedded in it
     """
+    def __init__(self,
+                 description,
+                 expected_value,
+                 score,
+                 query,             #Sequence from database
+                 query_id,           
+                 query_start,
+                 query_end,
+                 sbjct,             #Sequence that was blasted
+                 sbjct_id,           
+                 sbjct_start,
+                 sbjct_end):
+        
+        super(CoordXMLRecord,self).__init__(description,
+                                            expected_value,
+                                            score,
+                                            query,
+                                            query_id,           
+                                            query_start,
+                                            query_end,
+                                            sbjct,
+                                            sbjct_id,           
+                                            sbjct_start,
+                                            sbjct_end)
+        try:
+            reference,locus = location_reg.findall(self.sbjct_id)
+            self.genomeSt,self.genomeEnd,self.strand =  int(reference[0]), int(reference[1]), reference[2]
+            self.geneSt,self.geneEnd,self.geneStrand = int(locus[0]), int(locus[1]),locus[2]
+            self.query_start          +=self.genomeSt
+            self.query_end            +=self.genomeSt
+            self.organism = self.sbjct_id.split(":")[0]
+        except Exception as e:
+            print "Error: check your format\n",e
+            exit(0)
+
+    def __str__(self):
+        bacteriocinCoord = "%d-%d%s"%(self.query_start,self.query_end,self.strand)
+        geneCoord   = "%d-%d%s"%(self.geneSt,self.geneEnd,self.geneStrand)
+        return "%s\t%s\t%s\t%s\t%s"%(self.organism,geneCoord,bacteriocinCoord,self.query_id,self.query)
+
+class TabRecord():
+    """    Records information for each blast entry that is important for multiple alignment    """
     def __init__(self,
                  Query_id,
                  Subject_id,
@@ -167,6 +205,8 @@ class BLAST(object):
     def parseBLAST(self,mode):
         if mode=="xml":
             return self.parseXML()
+        elif mode=="coord":
+            return self.parseXML("coord")
         else:
             return self.parseTab()
 
@@ -202,30 +242,47 @@ class BLAST(object):
             print>>sys.stderr,e
             print>>sys.stderr,"No blast hits"
             raise
-    def parseXML(self):
+    def parseXML(self,mode=''):
         input_file = self.blastfile
         hits = []
         handle = open(input_file,'r')
         try:
             blast_hits = NCBIXML.parse(handle)
             blast_records = list(blast_hits)
-            i = 0
             for record in blast_records:
                 for alignment in record.alignments:
                     for hsp in alignment.hsps:
                         if hsp.expect<self.evalue:
-                            record=XMLRecord(record_number = i,
-                                             description = alignment.title,
-                                             expected_value = hsp.expect,
-                                             score = hsp.score,
-                                             query = hsp.query,
-                                             query_start = hsp.query_start,
-                                             query_end = hsp.query_end,
-                                             sbjct = hsp.sbjct,
-                                             sbjct_start = hsp.sbjct_start,
-                                             sbjct_end = hsp.sbjct_end)
-                            hits.append(record)
-        except:
+                            if mode=='coord':
+                                record=CoordXMLRecord(description = alignment.title,
+                                                      expected_value = hsp.expect,
+                                                      score = hsp.score,
+                                                      query_id = record.query,
+                                                      query = hsp.query,
+                                                      query_start = hsp.query_start,
+                                                      query_end = hsp.query_end,
+                                                      sbjct_id = alignment.hit_def,
+                                                      sbjct = hsp.sbjct,
+                                                      sbjct_start = hsp.sbjct_start,
+                                                      sbjct_end = hsp.sbjct_end)
+                                hits.append(record)
+                            else:
+                                record=XMLRecord(description = alignment.title,
+                                                 expected_value = hsp.expect,
+                                                 score = hsp.score,
+                                                 query_id = record.query,
+                                                 query = hsp.query,
+                                                 query_start = hsp.query_start,
+                                                 query_end = hsp.query_end,
+                                                 sbjct_id = alignment.hit_def,
+                                                 sbjct = hsp.sbjct,
+                                                 sbjct_start = hsp.sbjct_start,
+                                                 sbjct_end = hsp.sbjct_end)
+                                hits.append(record)
+
+
+        except Exception as e:
+            print>>sys.stderr,"Exception", e
             print>>sys.stderr,"No blast hits"
         return hits
 
@@ -313,6 +370,68 @@ if __name__=="__main__":
             records = blast_obj.parseBLAST('xml')
             self.assertEquals( len(records),1 )
             blast_obj.cleanup()
+
+
+    class TestBlastXML(unittest.TestCase):
+        def setUp(self):
+            self.refseq = "MAIVMGR*KGAR*"
+            self.fasta = "test.fa"
+            self.refid = "test"
+            test.createFasta(self.fasta,self.refid,self.refseq)
+            self.intermediate = "intermediate"
+            os.mkdir(self.intermediate)
+            self.evalue = 1
+        def tearDown(self):
+            try:
+                os.remove(self.fasta)
+                shutil.rmtree(self.intermediate)
+            except:
+                print "Nothing to remove"
+
+        def test1(self):
+            # seq = Seq("MAIVMGR*KGAR*")
+            # record = SeqRecord(seq,id="YP_025292.1", name="HokC",description="toxic membrane protein, small")
+            blast_obj = BLAST(self.fasta,self.fasta,self.intermediate,self.evalue)
+            blast_obj.buildDatabase("protein")
+            blast_obj.run(blast_cmd="blastp",num_threads=1)
+            records = blast_obj.parseBLAST('xml')
+            self.assertEquals( records[0].query_id , self.refid)
+            blast_obj.cleanup()
+
+
+    class TestBlastCoord(unittest.TestCase):
+        def setUp(self):
+            self.refseq = "MAIVMGR*KGAR*"
+            self.fasta = "test.fa"
+            self.refid = "test:1-10+|locus:2-5+"
+            test.createFasta(self.fasta,self.refid,self.refseq)
+
+            self.fasta1 = "test1.fa"
+            self.refid = "test:1-10+|locus:2-5+"
+            test.createFasta(self.fasta1,self.refid,self.refseq)
+
+            self.intermediate = "intermediate"
+            os.mkdir(self.intermediate)
+            self.evalue = 1
+
+        def tearDown(self):
+            try:
+                os.remove(self.fasta)
+                os.remove(self.fasta1)
+                shutil.rmtree(self.intermediate)
+            except:
+                print "Nothing to remove"
+
+        def test1(self):
+            # seq = Seq("MAIVMGR*KGAR*")
+            # record = SeqRecord(seq,id="YP_025292.1", name="HokC",description="toxic membrane protein, small")
+            blast_obj = BLAST(self.fasta,self.fasta1,self.intermediate,self.evalue)
+            blast_obj.buildDatabase("protein")
+            blast_obj.run(blast_cmd="blastp",num_threads=1)
+            records = blast_obj.parseBLAST('coord')
+            self.assertEquals( records[0].query_id , self.refid)
+            self.assertEquals( records[0].query_id , self.refid)
+            #blast_obj.cleanup()
 
 
     unittest.main()
